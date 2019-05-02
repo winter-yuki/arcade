d9f68fec4daee30793191295c3492a440aa8289b
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-05-02 21:58:00 +0300

    Example for outOfScene

--- arcanoid/main.cpp
+++ arcanoid/main.cpp
@@ -43,6 +43,10 @@ int main(int argc, char * argv[])
     collisions->setHandler(Engy::basicCollisionHandler);
 
     auto outOfScene = Engy::Controller::create<Engy::ECSceneBounds>(ball);
+    QObject::connect(outOfScene, &Engy::ECSceneBounds::isOut,
+                     [game = &game, outOfScene] {
+        game->removeEntity(outOfScene->entity());
+    });
 
     game.launch();
 
